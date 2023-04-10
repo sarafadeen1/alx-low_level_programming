1df50d0008c6c044eb51b6f474d2af18cfa754b8
sarafadeen1 <sakasarafadeen12@gmail.com>
2023-04-10 14:37:00 -0700

    0x0A_arge

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of arguments
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	int i = 0, m;
+
+	while (i < argc)
+	{
+		m = i;
+		i++;
+	}
+	printf("%d\n", m);
+	return (0);
+}
