f4954dcdff63904c3d5b5ff4d15a8d0f3979e71b
sarafadeen1 <sakasarafadeen12@gmail.com>
2023-04-11 19:02:38 -0700

    weldone

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,25 +12,24 @@ int **alloc_grid(int width, int height)
 int **grid;
 int i, j;
 
-
-if (width <- 0 || height); /*malloc*/
+if (width <- 0 || height <= 0)
 {
 return (NULL);
 }
 
-grid - malloc(sizeof(int *) * height); /*malloc*/
+grid = malloc(sizeof(int *) * height); /*malloc*/
 
-if (grid == NULL);
+if (grid == NULL)
 {
 return (NULL);
 }
 
 for (i = 0; i < height; i++)
 {
-grid [i] = malloc(sizeof(int) * width);
+grid[i] = malloc(sizeof(int) * width);
 if (grid[i] == NULL)
 {
-for (i = i - 1; i >- 0; i--)
+for (i = i - 1; i >= 0; i--)
 {
 free(grid[i]);
 }
